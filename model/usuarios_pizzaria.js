26dde599f864b305fe7c1234fa463f523324a095
zeurgoAlice <alicezcruz2018@gmail.com>
2023-11-24 13:49:09 -0300

    faltando bastante coisa ainda

---
+++ model/usuarios_pizzaria.js
@@ -0,0 +1,73 @@
+var usuarios =
+[
+   {
+    Nome: "Alice Tavares",
+    Email: "Alicetavares@gmail.com",
+    Senha: "luanapererecasuicida",
+    Endereço: [{
+
+          Estado: "SP",
+        Cidade: "Jandira",
+        Bairro: "Santa teresa",
+        Complemento: "Rua guarulhos 130"
+    }],
+         
+    Telefone: "11 97122-7890"
+},
+{
+    Nome: "Vitoria Alves",
+    Email: "Vitoriaalves@rhyta.com",
+    Senha: "12345678",
+    Endereço: [{
+
+          Estado: "BA",
+        Cidade: "Salvador",
+        Bairro: "xique xique",
+        Complemento: "4ª Travessa Mamede 1742"
+    }],
+         
+    Telefone: "71 3126-8865"
+},
+{
+    Nome: "Livia Azevedo",
+    Email: "liviaaraujoazevedo@gmail.com",
+    Senha: "liviaaaaa2121332",
+    Endereço: [{
+
+          Estado: "RS",
+        Cidade: "Novo Hamburgo",
+        Bairro: "Santa teresa",
+        Complemento: "Rua Tabatinga 441"
+    }],
+         
+    Telefone: "51 7067-8639"
+},
+{
+    Nome: "Alice Tavares",
+    Email: "Alicetavares@gmail.com",
+    Senha: "luanapererecasuicida",
+    Endereço: [{
+
+          Estado: "SP",
+        Cidade: "Jandira",
+        Bairro: "liv liv",
+        Complemento: "Rua guarulhos 130"
+    }],
+         
+    Telefone: "11 97122-7890"
+},
+{
+    Nome: "Letícia Costa",
+    Email: "Alicetavares@gmail.com",
+    Senha: "luanapererecasuicida",
+    Endereço: [{
+
+          Estado: "MT",
+        Cidade: "Cuiabá",
+        Bairro: "tim tim",
+        Complemento: "Rua L-5 1184"
+    }],
+         
+    Telefone: "11 97122-7890"
+} 
+] 
