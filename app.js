224eb779174591d513a9e6817f23e34540cc97a0
lice z <alicezcruz2018@gmail.com>
2023-11-28 17:30:59 -0300

    faltando as funções, imagens dos produtos e o app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+/************************************************************
+ * Objetivo: Trazer informações sobre os produtos da pizzaria: Tony-Pizzaria.
+ * Data: 24/11/2023
+ * Autor: Alice Zeurgo 
+ * Versão: 1.0
+ ***********************************************************/
+
+
+
---
+++ model/functions.js
@@ -0,0 +1,11 @@
+/************************************************************
+ * Objetivo: Trazer informações sobre os produtos da pizzaria: Tony-Pizzaria.
+ * Data: 24/11/2023
+ * Autor: Alice Zeurgo 
+ * Versão: 1.0
+ ***********************************************************/
+
+var produtos = require('./Produtos')
+const produtosJson = Produtos.Produtos
+
+
