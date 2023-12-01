f3a4ba113f49e43f1cb3fc970ccfed9d1977b145
zeurgoAlice <alicezcruz2018@gmail.com>
2023-12-01 17:28:14 -0300

    aaaaaaaaa

--- model/usuarios_pizzaria.js
+++ model/usuarios_pizzaria.js
@@ -1,9 +1,13 @@
+const { urlencoded } = require("body-parser");
+
 var usuarios =
 [
    {
+    Id: 1,
     Nome: "Alice Tavares",
     Email: "Alicetavares@gmail.com",
     Senha: "luanapererecasuicida",
+    Foto: "./img-usuarios/AliceTavares.jpg"
     Endereço: [{
 
           Estado: "SP",
@@ -15,9 +19,11 @@ var usuarios =
     Telefone: "11 97122-7890"
 },
 {
+    Id: 2,
     Nome: "Vitoria Alves",
     Email: "Vitoriaalves@rhyta.com",
     Senha: "12345678",
+    Foto: "./img-usuarios/VitoriaAlves.jpg",
     Endereço: [{
 
           Estado: "BA",
@@ -29,9 +35,11 @@ var usuarios =
     Telefone: "71 3126-8865"
 },
 {
+    Id: 3,
     Nome: "Livia Azevedo",
     Email: "liviaaraujoazevedo@gmail.com",
     Senha: "liviaaaaa2121332",
+    Foto: "./img-usuarios/LiviaAzevedo.jpg",
     Endereço: [{
 
           Estado: "RS",
@@ -43,31 +51,33 @@ var usuarios =
     Telefone: "51 7067-8639"
 },
 {
-    Nome: "Alice Tavares",
-    Email: "Alicetavares@gmail.com",
-    Senha: "luanapererecasuicida",
+    Nome: "Eduardo Zeurgo",
+    Email: "EduardoZeurgo@gmail.com",
+    Senha: "amoAaliceeee123",
+    Foto: "./img-usuarios/EduardoZeurgo.jpg",
     Endereço: [{
 
           Estado: "SP",
-        Cidade: "Jandira",
-        Bairro: "liv liv",
-        Complemento: "Rua guarulhos 130"
+        Cidade: "Barueri",
+        Bairro: "xiq xiq",
+        Complemento: "Rua cafundó 160"
     }],
          
-    Telefone: "11 97122-7890"
+    Telefone: "11 97165-7270"
 },
 {
     Nome: "Letícia Costa",
-    Email: "Alicetavares@gmail.com",
-    Senha: "luanapererecasuicida",
+    Email: "LetCosta@gmail.com",
+    Senha: "lrecasuicida",
+    Foto: "./img-usuarios/LeticiaCosta.jpg",
     Endereço: [{
 
           Estado: "MT",
         Cidade: "Cuiabá",
-        Bairro: "tim tim",
-        Complemento: "Rua L-5 1184"
+        Bairro: "lut put",
+        Complemento: "Rua L-2 1484"
     }],
          
-    Telefone: "11 97122-7890"
+    Telefone: "11 9583-7140"
 } 
 ] 
